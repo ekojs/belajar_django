c659d6cb8ccbe2fddec3b34e2b5bcf95c0c5a779
Eko Junaidi Salam <eko.junaidi.salam@gmail.com>
2021-01-31 16:29:29 +0700

    Less code is better

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,25 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
 
-def index(request):
-    latest_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_list': latest_list,}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_list'
 
-def detail(request, q_id):
-    q = get_object_or_404(Question, pk=q_id)
-    return render(request, 'polls/detail.html', {'q': q})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, q_id):
-    q = get_object_or_404(Question, pk=q_id)
-    return render(request, 'polls/results.html', {'q': q})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, q_id):
     q = get_object_or_404(Question, pk=q_id)
